0bca872aca8f6a0a08d5aad6b92fb7cfd9c8751d
rainara <rainara.n.moura@gmail.com>
2023-06-26 16:21:05 -0300

    Refactoring

--- perimetro/petimetro_test.go
+++ perimetro/petimetro_test.go
@@ -15,19 +15,23 @@ func TestPerimetro(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+
 	testesArea := []struct {
+		nome string
 		forma    Forma
 		esperado float64
 	}{
-		{forma: Retangulo{Largura: 12, Altura: 6}, esperado: 72.0},
-        {forma: Circulo{Raio: 10}, esperado: 314.1592653589793},
-        {forma: Triangulo{Base: 12, Altura: 6}, esperado: 36.0},
+		{nome: "Retângulo", forma: Retangulo{Largura: 12, Altura: 6}, esperado: 72.0},
+        {nome: "Círculo", forma: Circulo{Raio: 10}, esperado: 314.1592653589793},
+        {nome: "Triângulo", forma: Triangulo{Base: 12, Altura: 6}, esperado: 36.0},
 	}
 
 	for _, tt := range testesArea {
-		resultado := tt.forma.Area()
-		if resultado != tt.esperado {
-			t.Errorf("resultado %.2f, esperado %.2f", resultado, tt.esperado)
-		}
+		t.Run(tt.nome, func(t *testing.T) {
+			resultado := tt.forma.Area()
+			if resultado != tt.esperado {
+				t.Errorf("%#v resultado %.2f, esperado %.2f", tt.forma, resultado, tt.esperado)
+			}
+		})
 	}
 }

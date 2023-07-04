682e0bed9fb7236f4a2594c831e6ce3b8a29510c
rainara <rainara.n.moura@gmail.com>
2023-07-03 23:04:24 -0300

    Refactoring

--- mocks/contagem.go
+++ mocks/contagem.go
@@ -8,36 +8,64 @@ import (
 )
 
 type Sleeper interface {
-	Sleep()
+	Pausa()
 }
 
-type SleeperSpy struct {
-	Chamadas int
+type SpyContagemOperacoes struct {
+	Chamadas []string
 }
 
-type SleeperPadrao struct{}
+func (s *SpyContagemOperacoes) Pausa() {
+	s.Chamadas = append(s.Chamadas, pausa)
+}
+
+func (s *SpyContagemOperacoes) Write(p []byte) (n int, err error) {
+	s.Chamadas = append(s.Chamadas, escrita)
+	return
+}
+
+type SleeperConfiguravel struct {
+	duracao time.Duration
+	pausa func(time.Duration)
+}
+
+func (c *SleeperConfiguravel) Pausa() {
+	c.pausa(c.duracao)
+}
+
+
+type TempoSpy struct {
+	duracaoPausa time.Duration
+}
+
+
+func (t *TempoSpy) Pausa(duracao time.Duration) {
+	t.duracaoPausa = duracao
+}
+
+
 
 const ultimaPlavra = "Vai!"
 const inicioContagem = 3
+const escrita = "escrita"
+const pausa = "pausa"
 
-func (s *SleeperSpy) Sleep() {
-	s.Chamadas++
-}
-func (d *SleeperPadrao) Sleep() {
-	time.Sleep(1 * time.Second)
-}
 
 func Contagem(saida io.Writer, sleeper Sleeper) {
 	for i := inicioContagem; i > 0; i-- {
-		sleeper.Sleep()
+		sleeper.Pausa()
 		fmt.Fprintln(saida, i)
 	}
-	sleeper.Sleep()
+
+	for i := inicioContagem; i > 0; i -- {
+		fmt.Fprintln(saida, i)
+	}
+	sleeper.Pausa()
 	fmt.Fprint(saida, ultimaPlavra)
 
 }
 
 func main() {
-	sleeper := &SleeperPadrao{}
+	sleeper := &SleeperConfiguravel{1 * time.Second, time.Sleep}
 	Contagem(os.Stdout, sleeper)
 }
